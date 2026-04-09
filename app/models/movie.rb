34823e6b475f23479c87354502b4e889d27f5db9
Stephen McLaren <stephen-mclaren@hotmail.com>
2026-04-09 14:41:19 +0200

    models and validations complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,8 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks, dependent: :restrict_with_error
+  has_many :bookmarks
   has_many :lists, through: :bookmarks
 
-  validates :title, uniqueness: { scope: :movie_id }
+  validates :title, uniqueness: true
+  validates :title, presence: true
   validates :overview, presence: true
 end

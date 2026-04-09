5a4fe5c3224af5621eab582193fd11540e79749f
Stephen McLaren <stephen-mclaren@hotmail.com>
2026-04-09 16:08:01 +0200

    bookmark routes added, user can view lists and create list

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,17 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  
+  # get "lists", to: "lists#index"
+  # get "lists/new", to: "lists#new"
+  # get "lists/:id", to: "lists#show", as: :list
+  # post "lists", to: "lists#create"
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy ]
+
 end
